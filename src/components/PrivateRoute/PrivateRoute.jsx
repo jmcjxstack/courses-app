ace93630cd0f25e6f2fb11d283cf67d3e82ee957
Jose Miguel Carvajal Jimenez <josemiguel_carvajal@epam.com>
2023-11-15 09:53:36 -0500

    refactored Header component

--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navigate, Outlet } from 'react-router-dom';
+import { Outlet, Navigate } from 'react-router-dom';
 
 export default function PrivateRoute() {
 	const isAuthenticated = localStorage.getItem('isAuth') !== null;
